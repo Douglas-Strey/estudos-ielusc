6941b1566f9aedaa947023084330509a32c7f79f
Douglas Strey <douglas.morini@hotmail.com>
2021-08-16 17:39:07 -0300

    Esta é a versão sem upload de imagem

--- public/database/dataBase.php
+++ public/database/dataBase.php
@@ -10,18 +10,10 @@
         $produtoDescricao = $produto['productDescription'];
         $row = $produto['productPrice'];
         $produtoPreco = floatval($row);
-        $produtoImg = $_FILES['productImg']['tmp_name'];
-
-        // pegar imagem e converter pra base64
-        if ($produtoImg) {
-            $produtoImgBase64 = base64_encode(file_get_contents($produtoImg));
-        } else {
-            $produtoImgBase64 = 0;
-        }
     }
 
-    $sql = "INSERT INTO produtos (nome, descricao, preco, img) 
-            VALUES ('$produtoNome', '$produtoDescricao', '$produtoPreco', '$produtoImgBase64')";
+    $sql = "INSERT INTO produtos (nome, descricao, preco) 
+            VALUES ('$produtoNome', '$produtoDescricao', '$produtoPreco')";
 
     if (!$mysqli->query($sql)) :
         echo $mysqli->error;
