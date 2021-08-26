7d40d6f87eb448d111cb4b880a73a152a2c83084
Douglas Strey <douglas.morini@hotmail.com>
2021-08-26 14:13:21 -0300

    Atualizado JS do Product Data

--- public/src/js/products-page.js
+++ public/src/js/products-page.js
@@ -1,3 +1,42 @@
-/* $(document).ready(function () {
-    $('#productPrice').maskMoney();
-}); */
+function clearDataStorage() {
+    document.querySelector('.btnClearData').onclick = function () {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: 'btn btn-success popUpMargin',
+                cancelButton: 'btn btn-danger'
+            },
+            buttonsStyling: false
+        })
+
+        swalWithBootstrapButtons.fire({
+            title: 'Você tem certeza?',
+            text: "Você não será capaz de desfazer essa operação!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Sim, apague isso!',
+            cancelButtonText: 'Não, cancelar!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                swalWithBootstrapButtons.fire(
+                    'Apagado!',
+                    'Seus arquivos foram apagados com sucesso!',
+                    'success'
+                )
+                if (localStorage) {
+                    localStorage.clear();
+                }
+            } else if (
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons.fire(
+                    'Cancelado',
+                    'Seus arquivos continuam seguros :)',
+                    'error'
+                )
+            }
+        })
+    };
+};
+
+clearDataStorage();
